4b2345b1241ea417ac7b1e5d7de538b49ccbd52c
anish-18github <anishgaikar17@gmail.com>
2025-08-12 23:02:50 +0530

    Update log: Done with the Tab section, Now moving to Home page slider section :)

--- src/assets/js/global.js
+++ src/assets/js/global.js
@@ -6,3 +6,17 @@
 
 "use strict";
 
+
+/**
+ * Add event on multiple elements
+ * 
+ * @param {NodeList} $elements NodeList
+ * @param {string} eventType Event type string 
+ * @param {Function} callback callback function
+ */
+
+window.addEventOnElements = ($elements, eventType, callback) => {
+    for (const $element of $elements) {
+        $element.addEventListener(eventType, callback);
+    }
+}
